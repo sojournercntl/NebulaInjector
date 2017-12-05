2a6bd7ea28b273cfc284c84590a66145eca464f7
sojournercntl <marcus.ziegler@tmo.at>
2017-12-05 13:40:15 +0100

    Batch update

---
+++ nebula_update.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+REM WAIT FOR NEBULA TO CLOSE
+:loop
+tasklist | find "NebulaInjector" >nul
+if not errorlevel 1 (
+    timeout /t 10 >nul
+    goto :loop
+)
+
+REM COPY THE NEW VERSION
+COPY /Y "[NEBULAUPDATE]" "[NEBULANAME]"
+DEL "[NEBULAUPDATE]"
+
+REM START THE NEW VERSION
+cd [NEBULAEXEDIRECTORY]
+[NEBULANAME]
+
+REM DELETE THE FILE ITSELF
+(goto) 2>nul & del "%~f0"
