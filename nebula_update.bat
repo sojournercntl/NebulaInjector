c168dcce068624dde68442cb7b7fd001e2a28340
sojournercntl <marcus.ziegler@tmo.at>
2017-12-05 16:11:23 +0100

    Batch update

--- nebula_update.bat
+++ nebula_update.bat
@@ -1,17 +1,26 @@
 @ECHO OFF
-
 REM WAIT FOR NEBULA TO CLOSE
-:loop
-tasklist | find "NebulaInjector" >nul
-if not errorlevel 1 (
-    timeout /t 10 >nul
-    goto :loop
+REM :loop
+REM tasklist | find "NebulaInjector" >nul
+REM if not errorlevel 1 (
+REM     timeout /t 10 >nul
+REM     goto :loop
+REM )
+
+:LOOP
+PSLIST NebulaInjector >nul 2>&1
+IF ERRORLEVEL 1 (
+  GOTO CONTINUE
+) ELSE (
+  ECHO Nebula is still running
+  SLEEP 5
+  GOTO LOOP
 )
 
+:CONTINUE
 REM COPY THE NEW VERSION
 COPY /Y "[NEBULAUPDATE]" "[NEBULANAME]"
 
-DEL "[NEBULAUPDATE]"
 
 REM START THE NEW VERSION
 cd [NEBULAEXEDIRECTORY]
